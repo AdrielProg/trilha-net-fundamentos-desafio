88d6575254d0c83d9c1a27dd4f5a9de04bd80885
adriel alexander <adriel.alexs123@gmail.com>
2024-07-31 21:19:31 -0300

    implementing exception handling

--- DesafioFundamentos/Models/Veiculo.cs
+++ DesafioFundamentos/Models/Veiculo.cs
@@ -1,8 +1,28 @@
+using System.Text.RegularExpressions;
+using DesafioFundamentos.Exceptions;
+
 namespace DesafioFundamentos.Models
 {
     public class Veiculo
     {
-        public string Placa { get; set; }
+        private string _placa;
+        public string Placa
+        {
+            get
+            {
+                return _placa.ToUpper();
+            }
+
+            set
+            {
+                if (!ValidarPlaca(value))
+                {
+                    throw new InvalidArgumentException();
+                }
+                _placa = value;
+
+            }
+        }
         public DateTime Entrada { get; set; }
         public DateTime Saida { get; set; }
 
@@ -10,6 +30,13 @@ namespace DesafioFundamentos.Models
         public Veiculo(string placa)
         {
             Placa = placa;
+        }
+
+        public static bool ValidarPlaca(string placa)
+        {
+            string padrao = @"^[A-Z]{3}-\d{4}$";
+            Regex regex = new Regex(padrao);
+            return regex.IsMatch(placa.ToUpper());
         }
     }
 }
